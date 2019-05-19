ab7ce0754f1d869e442269b3edf8a5ce94f238f5
tyrone-zhao <tyrone-zhao@qq.com>
2019-05-19 15:40:12 +0800

    增加断点续传功能

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # m3u8MovieDownload
 下载m3u8电影并自动整合
+
+# 使用
+```python
+m3u8MovieDownload $ python3 m3u8Download.py -u https://zuikzy.603ee.com/2019/05/14/H5P4Z3whcCeiinjS/playlist.m3u8
+```
--- m3u8Download.py
+++ m3u8Download.py
@@ -5,17 +5,18 @@ import platform
 import requests
 import datetime
 from Crypto.Cipher import AES
+from pathlib import Path
 
 
 def download(url):
-    download_path = os.getcwd() + "\download"
+    download_path = os.getcwd() + "/download"
     if not os.path.exists(download_path):
         os.mkdir(download_path)
 
-    # 新建日期文件夹
-    download_path = os.path.join(download_path, datetime.datetime.now().strftime('%Y%m%d_%H%M%S'))
-    # print download_path
-    os.mkdir(download_path)
+    # 新建日期文件夹, datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+    download_path = os.path.join(download_path)
+    if not os.path.exists(download_path):
+        os.mkdir(download_path)
 
     print(f"开始下载视频，链接为:\n{url}")
     all_content = requests.get(url).text  # 获取第一层M3U8文件内容
@@ -27,50 +28,121 @@ def download(url):
         for line in file_line:
             if '.m3u8' in line:
                 url = url.rsplit("/", 1)[0] + "/" + line  # 拼出第二层m3u8的URL
+                with open("breakpoint.txt", "w") as f:
+                    f.write(url)
                 all_content = requests.get(url).text
 
     file_line = all_content.split("\n")
 
-    unknow = True
-    key = ""
-    for index, line in enumerate(file_line):  # 第二层
-        if "#EXT-X-KEY" in line:  # 找解密Key
-            method_pos = line.find("METHOD")
-            comma_pos = line.find(",")
-            method = line[method_pos:comma_pos].split('=')[1]
-            print("Decode Method：", method)
+    if not breakpointContinuingly(download_path, file_line, url):
+        unknow = True
+        key = ""
+        for index, line in enumerate(file_line):  # 第二层
+            if "#EXT-X-KEY" in line:  # 找解密Key
+                method_pos = line.find("METHOD")
+                comma_pos = line.find(",")
+                method = line[method_pos:comma_pos].split('=')[1]
+                print("Decode Method：", method)
 
-            uri_pos = line.find("URI")
-            quotation_mark_pos = line.rfind('"')
-            key_path = line[uri_pos:quotation_mark_pos].split('"')[1]
+                uri_pos = line.find("URI")
+                quotation_mark_pos = line.rfind('"')
+                key_path = line[uri_pos:quotation_mark_pos].split('"')[1]
 
-            key_url = url.rsplit("/", 1)[0] + "/" + key_path  # 拼出key解密密钥URL
-            res = requests.get(key_url)
-            key = res.content
-            print("key：", key)
+                key_url = url.rsplit("/", 1)[0] + "/" + key_path  # 拼出key解密密钥URL
+                res = requests.get(key_url)
+                key = res.content
+                print("key：", key)
 
-        if "EXTINF" in line:  # 找ts地址并下载
-            unknow = False
-            pd_url = url.rsplit("/", 1)[0] + "/" + file_line[index + 1]  # 拼出ts片段的URL
-            # print pd_url
+            if "EXTINF" in line:  # 找ts地址并下载
+                unknow = False
+                pd_url = url.rsplit("/", 1)[0] + "/" + file_line[index + 1]  # 拼出ts片段的URL
+                print(pd_url)
 
-            res = requests.get(pd_url)
-            c_fule_name = file_line[index + 1].rsplit("/", 1)[-1]
+                res = requests.get(pd_url)
+                c_fule_name = file_line[index + 1].rsplit("/", 1)[-1]
 
-            if len(key):  # AES 解密
-                cryptor = AES.new(key, AES.MODE_CBC, key)
-                with open(os.path.join(download_path, c_fule_name + ".mp4"), 'ab') as f:
-                    f.write(cryptor.decrypt(res.content))
-            else:
-                with open(os.path.join(download_path, c_fule_name), 'ab') as f:
-                    f.write(res.content)
-                    f.flush()
-    if unknow:
-        raise BaseException("未找到对应的下载链接")
+                if len(key):  # AES 解密
+                    cryptor = AES.new(key, AES.MODE_CBC, key)
+                    with open(os.path.join(download_path, c_fule_name + ".mp4"), 'ab') as f:
+                        f.write(cryptor.decrypt(res.content))
+                else:
+                    with open(os.path.join(download_path, c_fule_name), 'ab') as f:
+                        f.write(res.content)
+                        f.flush()
+        if unknow:
+            raise BaseException("未找到对应的下载链接")
+        else:
+            print("下载完成")
+
+        merge_file(download_path)
+
+
+def breakpointContinuingly(download_path, file_line, url):
+    """ 查找.m3u8文件 """
+    temp = []
+    try:
+        temp += [os.path.abspath(p) for p in Path(download_path).glob('**/*.ts')]
+    except PermissionError:
+        pass
+
+    if temp:
+        temp.sort()
+        print(temp)
+        filepath, tempfilename = os.path.split(temp[-1])
+        filename, extension = os.path.splitext(tempfilename)
+        print(tempfilename)
+
+        unknow = True
+        begin = True
+        key = ""
+        for index, line in enumerate(file_line):  # 第二层
+            if "#EXT-X-KEY" in line:  # 找解密Key
+                method_pos = line.find("METHOD")
+                comma_pos = line.find(",")
+                method = line[method_pos:comma_pos].split('=')[1]
+                print("Decode Method：", method)
+
+                uri_pos = line.find("URI")
+                quotation_mark_pos = line.rfind('"')
+                key_path = line[uri_pos:quotation_mark_pos].split('"')[1]
+
+                key_url = url.rsplit("/", 1)[0] + "/" + key_path  # 拼出key解密密钥URL
+                res = requests.get(key_url)
+                key = res.content
+                print("key：", key)
+
+            if "EXTINF" in line:  # 找ts地址并下载
+                if begin and tempfilename not in file_line[index + 1]:
+                    continue
+                else:
+                    print("开始断点续传！")
+                    begin = False
+
+                unknow = False
+                pd_url = url.rsplit("/", 1)[0] + "/" + file_line[index + 1]  # 拼出ts片段的URL
+                print(pd_url)
+
+                res = requests.get(pd_url)
+                c_fule_name = file_line[index + 1].rsplit("/", 1)[-1]
+
+                if len(key):  # AES 解密
+                    cryptor = AES.new(key, AES.MODE_CBC, key)
+                    with open(os.path.join(download_path, c_fule_name + ".mp4"), 'ab') as f:
+                        f.write(cryptor.decrypt(res.content))
+                else:
+                    with open(os.path.join(download_path, c_fule_name), 'ab') as f:
+                        f.write(res.content)
+                        f.flush()
+        if unknow:
+            raise BaseException("未找到对应的下载链接")
+        else:
+            print("下载完成")
+
+        merge_file(download_path)
     else:
-        print("下载完成")
+        return False
 
-    merge_file(download_path)
+    return True
 
 
 def merge_file(path):
